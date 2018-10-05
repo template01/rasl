4f18a74dd45baeca7c8addc98b77da372fb0731c
lasse <lasse@template-studio.nl>
2018-10-05 15:59:07 +0200

    yodates to single pages

--- pages/collection/cover/index.vue
+++ pages/collection/cover/index.vue
@@ -170,7 +170,7 @@ export default {
           contentsTitles = contentsTitles +this.selectedPosts[i].title.rendered
       }
 
-      return contentsTitles.substr(0, 500).trim() + '...';
+      return contentsTitles.substr(0, 525).trim() + '...';
 
     }
   },
