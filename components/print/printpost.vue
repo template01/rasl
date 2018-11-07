8dd16dd530b42447b4098e196d3dc49a8a63a33f
lasse <lasse@template-studio.nl>
2018-11-07 10:56:26 +0100

    by authors fix in read

--- components/print/printpost.vue
+++ components/print/printpost.vue
@@ -82,7 +82,7 @@ export default {
             -webkit-transform: rotate(90deg);
             transform: rotate(90deg);
             // EQAL HOOGTE VAN WRAPPER
-            margin-left: 18px;
+            margin-left: 52px;
 
             -webkit-transform-origin: top left;
             transform-origin: top left;
